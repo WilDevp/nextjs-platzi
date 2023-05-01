06550195c992cf3f524c560142405202e92b82b1
WilDevp <wilmar.garciava@gmail.com>
2023-04-30 20:46:45 -0500

    rutas dianmicas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d3a2b06a0e68c31"; }
+/******/ 		__webpack_require__.h = function() { return "adb72cb6daf6ab43"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
